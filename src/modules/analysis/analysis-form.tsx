3aa6cc6e673d078a6d66e4a318eed711e20258e9
Radosław Rzepka <radoslawrzepka02@gmail.com>
2023-08-02 16:26:09 +0200

    feat: change navigation link color

---
+++ src/modules/analysis/analysis-form.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+interface AnalysisFormProps {}
+
+const AnalysisForm: FC<AnalysisFormProps> = ({}) => {
+   return (
+      <form>
+         <h1 className="text-6xl">Start your analysis</h1>
+      </form>
+   );
+};
+
+export default AnalysisForm;
